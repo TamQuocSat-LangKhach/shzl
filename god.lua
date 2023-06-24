d75c306ad44f2f9b59ae3c35edef083dd5066b6d
Mechanel <nyutanislavsky@qq.com>
2023-06-25 01:25:37 +0800

    add godliubei

--- god.lua
+++ god.lua
@@ -1140,7 +1140,7 @@ Fk:loadTranslationTable{
   [":jilue"] = "你可以弃置1枚“忍”，发动下列一项技能：〖鬼才〗、〖放逐〗、〖集智〗、〖制衡〗、〖完杀〗。",
 }
 
---[[local godliubei = General(extension, "godliubei", "god", 6)
+local godliubei = General(extension, "godliubei", "god", 6)
 local longnu = fk.CreateTriggerSkill{
   name = "longnu",
   events = {fk.EventPhaseStart},
@@ -1166,11 +1166,11 @@ local longnu = fk.CreateTriggerSkill{
 local longnu_filter = fk.CreateFilterSkill{
   name = "#longnu_filter",
   card_filter = function(self, to_select, player)
-    if not table.contains(player.player_cards[Player.Hand], to_select.id) or player:getMark("_longnu-phase") == 0 then return false end
+    if player:getMark("_longnu-phase") == 0 or table.contains(player.player_cards[Player.Equip], to_select.id) or table.contains(player.player_cards[Player.Judge], to_select.id) then return false end
     return (player:getMark("_longnu-phase") == "yang" and to_select.color == Card.Red) or (player:getMark("_longnu-phase") == "yin" and to_select.type == Card.TypeTrick)
   end,
   view_as = function(self, to_select, player)
-    local card = Fk:cloneCard(player:getMark("_longnu-phase") == 1 and "fire__slash" or "thunder__slash", to_select.suit, to_select.number)
+    local card = Fk:cloneCard(player:getMark("_longnu-phase") == "yang" and "fire__slash" or "thunder__slash", to_select.suit, to_select.number)
     card.skillName = self.name
     return card
   end,
@@ -1180,7 +1180,7 @@ local longnu_targetmod = fk.CreateTargetModSkill{
   distance_limit_func =  function(self, player, skill, card)
     return (player:getMark("_longnu-phase") == "yang" and skill.trueName == "slash_skill" and card.name == "fire__slash") and 999 or 0
   end,
-  residue_func = function(self, player, skill, scope, 2)
+  residue_func = function(self, player, skill, scope, card)
     return (player:getMark("_longnu-phase") == "yin" and skill.trueName == "slash_skill" and scope == Player.HistoryPhase and card.name == "thunder__slash") and 999 or 0
   end,
 }
@@ -1240,8 +1240,15 @@ Fk:loadTranslationTable{
   ["jieying"] = "结营",
   [":jieying"] = "锁定技，你始终处于横置状态；处于连环状态的角色手牌上限+2；结束阶段开始时，你横置一名其他角色。",
 
+  ["#longnu_filter"] = "龙怒",
   ["#jieying-target"] = "结营：选择一名其他角色，令其横置",
-}]]
+
+  ["$longnu1"] = "损身熬心，誓报此仇！",
+  ["$longnu2"] = "兄弟疾难，血债血偿！",
+  ["$jieying1"] = "桃园结义，营一世之交。",
+  ["$jieying2"] = "结草衔环，报兄弟大恩。",
+  ["~godliubei"] = "桃园依旧，来世再结……",
+}
 
 Fk:loadTranslationTable{
   ["godluxun"] = "神陆逊",
