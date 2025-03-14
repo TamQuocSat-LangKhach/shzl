fc899307e8aa0417335aba0bfb86a4fee48e51cc
RalphR <1715333874@qq.com>
2025-03-14 13:21:31 +0800

    fix祸首

--- pkg/forest/skills/huoshou.lua
+++ pkg/forest/skills/huoshou.lua
@@ -21,7 +21,7 @@ huoshou:addEffect(fk.PreCardEffect, {
   end,
 })
 huoshou:addEffect(fk.TargetSpecified, {
-  anim_type = "defensive",
+  anim_type = "offensive",
   can_trigger = function(self, event, target, player, data)
     return target ~= player and player:hasSkill(huoshou.name) and data.card.trueName == "savage_assault" and data.firstTarget
   end,
@@ -35,7 +35,7 @@ huoshou:addEffect(fk.PreDamage, {
     if data.card and data.card.trueName == "savage_assault" then
       local e = player.room.logic:getCurrentEvent():findParent(GameEvent.UseCard)
       if e then
-        local use = e.data[1]
+        local use = e.data
         return use.extra_data and use.extra_data.huoshou
       end
     end
@@ -44,8 +44,9 @@ huoshou:addEffect(fk.PreDamage, {
     local room = player.room
     local e = room.logic:getCurrentEvent():findParent(GameEvent.UseCard)
     if e then
-      local use = e.data[1]
-      data.from = room:getPlayerById(use.extra_data.huoshou)
+      local use = e.data
+      local from = room:getPlayerById(use.extra_data.huoshou)
+      data.from = not from.dead and from or nil
     end
   end,
 })
